663e296754bac4f5504365d62ca11eea1422d27a
sprawah <samata.prawah@gmail.com>
2018-02-27 12:46:39 -0600

    Added test case-2 to validate Content-Type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -16,6 +17,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     public void mavenFlowReturnsHelloMaven() throws Exception {
     	System.out.println("\n\nTestcase-1: Dynamic port ----------->" + myPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Override
